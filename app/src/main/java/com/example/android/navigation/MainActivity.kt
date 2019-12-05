e30ec6cf892264cfacaffb296f48954a44b86212
gortiz7941 <43448633+gortiz7941@users.noreply.github.com>
2019-12-04 20:10:21 -0500

    added up button in app bar, add options menu, added navigation drawer

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,13 +19,33 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // member var to represent drawer layout
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // initialize drawerLayout
+        drawerLayout = binding.drawerLayout
+        // code to find navigation object
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        // link navController to app bar
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        // allows user to dispaly navigation drawer
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+    // override to call navigateUp()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
     // TODO (01) Create the new TitleFragment
